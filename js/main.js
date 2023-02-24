b607ad006477882a5ff8447fe0a9ea353359977e
Valerio Salzano <vlr.slzn@gmail.com>
2023-02-24 15:41:50 +0100

    basic job, bonus1&2 done, working on responsive

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const squaresContainer = document.querySelector('.squares-container');
+
+for (let i = 1; i <= 100 ; i++ ){
+    // squaresContainer.innerHTML += `<div class="square" id="square-${i}"></div>`
+    const square = document.createElement("div");
+    const squareText = document.createElement("span")
+
+    // let square = document.getElementById(`square-${i}`);
+    square.classList.add('square')
+    square.id = (`square-${i}`)
+    square.append(squareText)
+    squaresContainer.append(square);
+
+    if (i % 3 == 0 || i % 5 == 0){
+        if ( i % 3 == 0){
+            square.classList.add("fizz");
+            squareText.innerHTML += "Fizz";
+        }
+        if ( i % 5 == 0){
+            square.classList.add("buzz");
+            squareText.innerHTML += "Buzz";
+        }
+    }else {
+        squareText.innerHTML = i;
+    }
+}
