1db08ccedf0eb1afa55e26ca5041dd200f6075d7
Valerio Salzano <vlr.slzn@gmail.com>
2023-02-24 15:50:47 +0100

    added missing semicolons

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 const squaresContainer = document.querySelector('.squares-container');
 
 for (let i = 1; i <= 100 ; i++ ){
+    
     // squaresContainer.innerHTML += `<div class="square" id="square-${i}"></div>`
-    const square = document.createElement("div");
-    const squareText = document.createElement("span")
-
     // let square = document.getElementById(`square-${i}`);
-    square.classList.add('square')
-    square.id = (`square-${i}`)
-    square.append(squareText)
+
+    const square = document.createElement("div");
+    const squareText = document.createElement("span");
+
+    square.classList.add('square');
+    square.id = (`square-${i}`);
+    square.append(squareText);
     squaresContainer.append(square);
 
     if (i % 3 == 0 || i % 5 == 0){
